a6d4300f58001cc41c1d386f7e2599fa286f1a5c
James George <jamesgeorge998001@gmail.com>
2018-05-11 14:07:46 +0530

    Bug fixes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
   render() {
     return (
       <div>
-      <h1 style = {{fontSize: 50, textAlign: 'center', fontFamily: 'times'}}>Time you had spend here:</h1>
+      <h1 style = {{fontSize: 80, textAlign: 'center', fontFamily: 'times'}}>Timer</h1>
       <Timer/>
       </div>
     );
--- src/Timer.js
+++ src/Timer.js
@@ -7,21 +7,24 @@ export default class Timer extends Component{
   constructor(props){
     super(props);
 
-    this.state = {time: 0};
-    this.pause_disabled = true;
-    this.reset_disabled = true;
-    this.start_button = 'Start';
-    this.pause_button = 'Pause';
-    this.reset_button = 'Reset';
+    this.state = {time: 0,
+    pause_disabled : true,
+    reset_disabled : true,
+    start_button : 'Start',
+    pause_button : 'Pause',
+    reset_button : 'Reset'};
+
     this.startTimer = this.startTimer.bind(this);
     this.changeState = this.changeState.bind(this);
     this.pauseTimer = this.pauseTimer.bind(this);
     this.resetTimer = this.resetTimer.bind(this);
   }
   startTimer(){
-    this.start_disabled = true;
-    this.pause_disabled =  false;
-    this.start_button = 'Start';
+    this.setState({
+    start_disabled : true,
+    pause_disabled :  false,
+    reset_disabled : true,
+    start_button : 'Start'});
     this.timer = setInterval(this.changeState, 1000);
   }
   changeState(){
@@ -29,29 +32,26 @@ export default class Timer extends Component{
     console.log(this.state);
   }
   pauseTimer(){
-    this.start_disabled = false;
-    this.pause_disabled = true;
-    this.reset_disabled = false;
-    this.start_button = 'Resume';
-    this.clear();
-  }
-  clear(){
+    this.setState({start_disabled : false,
+    pause_disabled : true,
+    reset_disabled : false,
+    start_button : 'Resume'
+    });
     clearInterval(this.timer);
   }
   resetTimer(){
-    this.reset_disabled = true;
-    this.setState({time: 0});
+    this.setState({time: 0, reset_disabled : true, start_button : 'Start'});
   }
 
   render(){
     return (
       <div>
-        <h1 style={{fontSize: 160, textAlign: 'center'}}>{this.state.time} s</h1>
+        <h1 style={{fontSize: 120, textAlign: 'center'}}>{this.state.time} s</h1>
         <br />
         <center>
-        <button type = 'button' onClick = {this.startTimer} style = {{fontSize: 50, borderRadius: 10}} disabled = {this.start_disabled}>{this.start_button}</button>
-        <button type = 'button' onClick = {this.pauseTimer} style = {{fontSize: 50, borderRadius: 10}} disabled = {this.pause_disabled}>{this.pause_button}</button>
-        <button type = 'button' onClick = {this.resetTimer} style = {{fontSize: 50, borderRadius: 10}} disabled = {this.reset_disabled}>{this.reset_button}</button>
+        <button type = 'button' onClick = {this.startTimer} style = {{fontSize: 50, borderRadius: 10}} disabled = {this.state.start_disabled}>{this.state.start_button}</button>
+        <button type = 'button' onClick = {this.pauseTimer} style = {{fontSize: 50, borderRadius: 10}} disabled = {this.state.pause_disabled}>{this.state.pause_button}</button>
+        <button type = 'button' onClick = {this.resetTimer} style = {{fontSize: 50, borderRadius: 10}} disabled = {this.state.reset_disabled}>{this.state.reset_button}</button>
         </center>
       </div>
     );
