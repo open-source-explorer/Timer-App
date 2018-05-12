3993efdf5c7806ed990024cbc95a714142aabae3
James George <jamesgeorge998001@gmail.com>
2018-05-12 08:01:43 +0530

    modifications related to showing time.

--- src/Timer.js
+++ src/Timer.js
@@ -7,17 +7,17 @@ export default class Timer extends Component{
   constructor(props){
     super(props);
 
-    this.state = {time: 0,
+    this.state = {time: {'hours':0, 'minutes':0, 'seconds':0},
     pause_disabled : true,
     reset_disabled : true,
     start_button : 'Start',
     pause_button : 'Pause',
     reset_button : 'Reset'};
 
-    this.startTimer = this.startTimer.bind(this);
+    /*this.startTimer = this.startTimer.bind(this);
     this.changeState = this.changeState.bind(this);
     this.pauseTimer = this.pauseTimer.bind(this);
-    this.resetTimer = this.resetTimer.bind(this);
+    this.resetTimer = this.resetTimer.bind(this);*/
   }
   startTimer(){
     this.setState({
@@ -28,7 +28,19 @@ export default class Timer extends Component{
     this.timer = setInterval(this.changeState, 1000);
   }
   changeState(){
-    this.setState({time: this.state.time+=1});
+    if(this.state.time['seconds'] <= 59){
+    this.setState({time['seconds'] : this.state.time['seconds'] += 1});
+    }
+    else{
+      this.setState({time['seconds'] : 0, time['minutes'] : this.state.time['minutes'] += 1);
+      if(this.state.time['minutes'] > 59){
+        this.setState({time['seconds'] : 0, time['minutes'] : 0, time['hours'] : this.state.time['hours'] += 1});
+        if(this.state.time['hours'] > 24){
+          this.setState({time['hours']: 0});
+        }
+      }
+    }
+
     console.log(this.state);
   }
   pauseTimer(){
@@ -46,7 +58,7 @@ export default class Timer extends Component{
   render(){
     return (
       <div>
-        <h1 style={{fontSize: 120, textAlign: 'center'}}>{this.state.time} s</h1>
+        <h1 style={{fontSize: 120, textAlign: 'center'}}>{this.state.time['hours']} : {this.state.time['minutes']} : {this.state.time['seconds']} s</h1>
         <br />
         <center>
         <button type = 'button' onClick = {this.startTimer} style = {{fontSize: 50, borderRadius: 10}} disabled = {this.state.start_disabled}>{this.state.start_button}</button>
